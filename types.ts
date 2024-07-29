07f6953cd35ea7e9afe4e051a143509ea8cf1829
Lucas Pineda <lucas.pineda@hotmail.com>
2024-07-29 20:18:46 -0300

    add song types

---
+++ types.ts
@@ -0,0 +1,22 @@
+export interface Album {
+  title: string;
+  year: number;
+}
+
+export interface File {
+  audio: string;
+  coverArt: string;
+  poster: string;
+}
+
+export interface SongDetails {
+  album: Album;
+  artist: string;
+  files: File;
+  title: string;
+}
+
+export interface Song {
+  id: number;
+  song: SongDetails;
+}
